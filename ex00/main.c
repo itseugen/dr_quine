7af8883996d1edc6c80b97f8de2677eb5f8298a8
Eugen <eugen.weiberle@gmail.com>
2026-01-29 21:12:16 +0100

    A baby quine

--- ex00/main.c
+++ ex00/main.c
@@ -2,6 +2,8 @@
 
 int	main()
 {
-	printf("Hello world\n");
+	char	*str =
+	"#include <stdio.h>%c%cint	main()%c{%c	char	*str =%c	%c%s%c;%c	printf(str, 10, 10, 10, 10, 10, 34, str, 34, 10, 10, 10, 10);%c	return (0);%c}%c";
+	printf(str, 10, 10, 10, 10, 10, 34, str, 34, 10, 10, 10, 10);
 	return (0);
 }
