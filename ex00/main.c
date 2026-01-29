3bf2652b81691e48f29551f3b13c08c238955951
Eugen <eugen.weiberle@gmail.com>
2026-01-29 21:27:52 +0100

    Changing the comment style to satisfy the demands (that definitely could've been on the actual exercise where we need them grrrrr)

--- ex00/main.c
+++ ex00/main.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
-// A comment about how my need for newlines makes this project needlessly more complex looking
+/*
+* A comment about how my need for newlines makes this project needlessly more complex looking
+*/
 
 int	function_without_much_use()
 {
-	// Inline comment
+	/*
+	* Inline comment
+	*/
 	return (0);
 }
 
 int	main()
 {
 	char	*str =
-	"#include <stdio.h>%c%c// A comment about how my need for newlines makes this project needlessly more complex looking%c%cint	function_without_much_use()%c{%c	// Inline comment%c	return (0);%c}%c%cint	main()%c{%c	char	*str =%c	%c%s%c;%c	printf(str, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, str, 34, 10, 10, 10, 10);%c	return (0);%c}%c";
-	printf(str, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, str, 34, 10, 10, 10, 10);
+	"#include <stdio.h>%c%c/*%c* A comment about how my need for newlines makes this project needlessly more complex looking%c*/%c%cint	function_without_much_use()%c{%c	/*%c	* Inline comment%c	*/%c	return (0);%c}%c%cint	main()%c{%c	char	*str =%c	%c%s%c;%c	printf(str, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, str, 34, 10, 10, 10, 10);%c	return (0);%c}%c";
+	printf(str, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, str, 34, 10, 10, 10, 10);
 	return (0);
 }
